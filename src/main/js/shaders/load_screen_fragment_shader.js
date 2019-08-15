f68fe1e99dc6d92cbd4130c762c60a82be4448f6
scotthare <the.sh.in.it@gmail.com>
2019-08-15 14:58:34 +0100

    changed colour to green

--- src/main/js/shaders/load_screen_fragment_shader.js
+++ src/main/js/shaders/load_screen_fragment_shader.js
@@ -24,9 +24,8 @@ export function fragmentSource() {
             float r = length(uv) * 2.0;
             //compact loader icon from IQ
             float a = atan(uv.y, uv.x) + 1.0 * u_time;      
-            pc = mix(pc, vec3(1.0, 1.0, 1.0), 
+            pc = mix(pc, vec3(0.0, 1.0, 0.0), 
                      (1.0 - smoothstep(0.10, 0.14, abs(r - 0.5)))  * smoothstep(0.4, 0.6, sin(17.0 * a)));
-
                      
             outputColour = vec4(pc, 1.0);
         }
